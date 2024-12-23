100233b01d40e602955b86c61aedefa2f6f35c0b
Franklyn Vasquez <franklynvasquez24@gmail.com>
2024-12-23 14:00:45 +0200

    Added support as a WindowsService

--- CLIService/Program.cs
+++ CLIService/Program.cs
@@ -1,5 +1,17 @@
 using CLIService;
+#if WINDOWS
+using Microsoft.Extensions.Logging.Configuration;
+using Microsoft.Extensions.Logging.EventLog;
+#endif
 
 HostApplicationBuilder builder = Host.CreateApplicationBuilder(args);
+#if WINDOWS
+builder.Services.AddWindowsService(options =>
+{
+    options.ServiceName = "PanelControllerCLIService";
+});
+#endif
+
 builder.Services.AddHostedService<CLIWorker>();
-builder.Build().Run();
+IHost host = builder.Build();
+host.Run();
